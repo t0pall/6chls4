18b335a6857a6ab90b9b5b3d60d2f5667928044d
Igor Topal <i@itopal.ru>
2023-10-12 18:25:58 +0300

    add worm

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [result, setResult] = useState('');
+  const onChange = (e) => {
+    let data = e.target?.value?.split('\n');
+    data = data.map((element) => {
+      return `https://supchat.taxi.yandex-team.ru/chat/${element}`;
+    });
+    setResult(data.join('\n'));
+  };
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <textarea onChange={onChange} style={{ height: '200px', width: "200px", fontSize: "7px" }}  />
+          <textarea value={result} style={{ height: '200px', width: "300px", fontSize: "7px" }} />
+        </div>
+        <p>ОК на пуcтой!</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
